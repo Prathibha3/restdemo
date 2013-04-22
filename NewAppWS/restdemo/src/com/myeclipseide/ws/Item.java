3104a16506f682573478f1cb63ad63db763b9e25
Prathibha <praths2011@hotmail.com>
2013-04-22 10:41:39 -0700

    Files for the war file and directory structure

---
+++ NewAppWS/restdemo/src/com/myeclipseide/ws/Item.java
@@ -0,0 +1,56 @@
+package com.myeclipseide.ws;
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Item implements Serializable{
+	private int id;
+	private String Storename;
+	private String location;
+	private String itemdescription;
+	private int price;
+	private String currency;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStorename() {
+		return Storename;
+	}
+	public void setStorename(String storename) {
+		this.Storename=storename;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getItemdescription() {
+		return itemdescription;
+	}
+	public void setItemdescription(String itemdescription) {
+		this.itemdescription = itemdescription;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency=currency;
+	}
+
+	
+	
+	
+	
+
+}
